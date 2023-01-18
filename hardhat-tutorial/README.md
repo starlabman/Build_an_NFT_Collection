b1990659a09354ebe62f778b1dec8e0c1f62e30a
labore <akodjolabore@gmail.com>
2023-01-18 18:44:50 +0000

    test

---
+++ hardhat-tutorial/README.md
@@ -0,0 +1,14 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
+0x3d8fDf20E86D68474154D3cD47b4a48c394fFB03
---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x83570eB022d2C682eb4Fa3e4Be785cf288700fbc";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://github.com/starlabman/Build-a-whitelist-dApp-for-your-upcoming-NFT-Collection/tree/1ab3d149beda419d2b35a4e52954b6ef456abbac/Whitelist-Dapp/whitelist-front/pages/api";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
